1eb8b0e27c91357a52eaee9bc639151d6f9c78c6
Petr Filaretov <p.filaretov@okko.tv>
2022-09-09 08:43:04 +0300

    initial commit

---
+++ src/main/kotlin/pro/filaretov/spring/kotlin/kj17s/Kj17sApplication.kt
@@ -0,0 +1,11 @@
+package pro.filaretov.spring.kotlin.kj17s
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Kj17sApplication
+
+fun main(args: Array<String>) {
+    runApplication<Kj17sApplication>(*args)
+}
---
+++ src/test/kotlin/pro/filaretov/spring/kotlin/kj17s/Kj17sApplicationTests.kt
@@ -0,0 +1,13 @@
+package pro.filaretov.spring.kotlin.kj17s
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Kj17sApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
